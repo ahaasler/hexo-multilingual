66e82c469ad5ef8de94d4d2a2d9feaa02a3ff267
Adrian Haasler García <dev@adrianhaasler.com>
2015-11-17 20:02:22 +0000

    Add filter to localize page config values

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 exports.util = require('./lib/util');
 
 if (typeof hexo !== 'undefined') {
+  // Filter
+  require('./lib/filter')(hexo);
   // Generators
   require('./lib/generator')(hexo);
   // Helpers
